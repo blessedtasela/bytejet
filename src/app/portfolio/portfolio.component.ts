a3b39e83d67b94ffb5cc018a15b77b7264ea81ea
Blessed Tasela <taselablessed@gmail.com>
2024-04-16 14:49:45 -0700

    added scroll to top component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
 import { LandingComponent } from '../landing/landing.component';
+import { ContactComponent } from '../contact/contact.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { ScrollToTopComponent } from '../scroll-to-top/scroll-to-top.component';
+import { ServicesComponent } from '../services/services.component';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,7 +12,11 @@ import { LandingComponent } from '../landing/landing.component';
   imports: [
     AboutComponent,
     LandingComponent,
-    
+    ContactComponent,
+    ProjectsComponent,
+    ScrollToTopComponent,
+    ServicesComponent,
+
   ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
